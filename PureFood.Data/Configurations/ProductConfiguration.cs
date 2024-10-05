a89b6a27244d7d25b97d222a8ff5133044b8d9a6
QuocDai <145921869+dainq94@users.noreply.github.com>
2024-10-05 13:32:51 +0700

    add expireTime and EndTime

--- PureFood.Data/Configurations/ProductConfiguration.cs
+++ PureFood.Data/Configurations/ProductConfiguration.cs
@@ -30,6 +30,8 @@ namespace PureFood.Data.Configurations
                     Status = true,
                     CreatedAt = DateTime.Parse("2024-10-02"),
                     UpdatedAt = DateTime.Parse("2024-10-02"),
+                    EntryDate = new DateTime(2024, 10, 19), // 19th October 2024
+                    ExpiryDate = new DateTime(2024, 11, 15) // 15th November 2024
                 },
 
                 new Product()
@@ -50,6 +52,8 @@ namespace PureFood.Data.Configurations
                     Status = true,
                     CreatedAt = DateTime.Parse("2024-10-02"),
                     UpdatedAt = DateTime.Parse("2024-10-02"),
+                     EntryDate = new DateTime(2024, 10, 20), // 20th October 2024
+                     ExpiryDate = new DateTime(2024, 11, 16) // 16th November 2024
                 },
 
                 new Product()
@@ -70,6 +74,8 @@ namespace PureFood.Data.Configurations
                     Status = true,
                     CreatedAt = DateTime.Parse("2024-10-02"),
                     UpdatedAt = DateTime.Parse("2024-10-02"),
+                    EntryDate = new DateTime(2024, 10, 21), // 21st October 2024
+                     ExpiryDate = new DateTime(2024, 11, 17) // 17th November 2024
                 },
 
                 new Product()
@@ -90,6 +96,8 @@ namespace PureFood.Data.Configurations
                     Status = true,
                     CreatedAt = DateTime.Parse("2024-10-02"),
                     UpdatedAt = DateTime.Parse("2024-10-02"),
+                      EntryDate = new DateTime(2024, 10, 22), // 22nd October 2024
+                     ExpiryDate = new DateTime(2024, 11, 18)
                 },
 
                 new Product()
@@ -110,6 +118,8 @@ namespace PureFood.Data.Configurations
                     Status = true,
                     CreatedAt = DateTime.Parse("2024-10-02"),
                     UpdatedAt = DateTime.Parse("2024-10-02"),
+                     EntryDate = new DateTime(2024, 10, 23), // 23rd October 2024
+                    ExpiryDate = new DateTime(2024, 11, 19) // 19th November 2024
                 },
 
                 new Product()
@@ -130,6 +140,8 @@ namespace PureFood.Data.Configurations
                     Status = true,
                     CreatedAt = DateTime.Parse("2024-10-02"),
                     UpdatedAt = DateTime.Parse("2024-10-02"),
+                      EntryDate = new DateTime(2024, 10, 24), // 24th October 2024
+                     ExpiryDate = new DateTime(2024, 11, 20) // 20th November 2024
                 },
 
                 new Product()
@@ -150,6 +162,8 @@ namespace PureFood.Data.Configurations
                     Status = true,
                     CreatedAt = DateTime.Parse("2024-10-02"),
                     UpdatedAt = DateTime.Parse("2024-10-02"),
+                     EntryDate = new DateTime(2024, 10, 25), // 25th October 2024
+                     ExpiryDate = new DateTime(2024, 11, 21) // 21st November 2024
                 },
 
                 new Product()
@@ -170,6 +184,8 @@ namespace PureFood.Data.Configurations
                     Status = true,
                     CreatedAt = DateTime.Parse("2024-10-02"),
                     UpdatedAt = DateTime.Parse("2024-10-02"),
+                     EntryDate = new DateTime(2024, 10, 26), // 26th October 2024
+                     ExpiryDate = new DateTime(2024, 11, 22) // 22nd November 2024
                 },
 
                 new Product()
@@ -190,6 +206,8 @@ namespace PureFood.Data.Configurations
                     Status = true,
                     CreatedAt = DateTime.Parse("2024-10-02"),
                     UpdatedAt = DateTime.Parse("2024-10-02"),
+                        EntryDate = new DateTime(2024, 10, 27), // 27th October 2024
+                    ExpiryDate = new DateTime(2024, 11, 23) // 23rd November 2024
                 },
 
                 new Product()
@@ -210,6 +228,8 @@ namespace PureFood.Data.Configurations
                     Status = true,
                     CreatedAt = DateTime.Parse("2024-10-02"),
                     UpdatedAt = DateTime.Parse("2024-10-02"),
+                      EntryDate = new DateTime(2024, 10, 28), // 28th October 2024
+                    ExpiryDate = new DateTime(2024, 11, 24) // 24th November 2024
                 },
 
                 new Product()
@@ -230,6 +250,8 @@ namespace PureFood.Data.Configurations
                     Status = true,
                     CreatedAt = DateTime.Parse("2024-10-02"),
                     UpdatedAt = DateTime.Parse("2024-10-02"),
+                     EntryDate = new DateTime(2024, 10, 29), // 29th October 2024
+            ExpiryDate = new DateTime(2024, 11, 25) // 25th November 2024
                 },
 
                 new Product()
@@ -250,6 +272,8 @@ namespace PureFood.Data.Configurations
                     Status = true,
                     CreatedAt = DateTime.Parse("2024-10-02"),
                     UpdatedAt = DateTime.Parse("2024-10-02"),
+                     EntryDate = new DateTime(2024, 10, 30), // 30th October 2024
+            ExpiryDate = new DateTime(2024, 11, 26) // 26th November 2024
                 },
 
                 new Product()
@@ -270,6 +294,8 @@ namespace PureFood.Data.Configurations
                     Status = true,
                     CreatedAt = DateTime.Parse("2024-10-02"),
                     UpdatedAt = DateTime.Parse("2024-10-02"),
+                      EntryDate = new DateTime(2024, 10, 31), // 31st October 2024
+            ExpiryDate = new DateTime(2024, 11, 27) // 27th November 2024
                 },
 
                 new Product()
@@ -290,6 +316,8 @@ namespace PureFood.Data.Configurations
                     Status = true,
                     CreatedAt = DateTime.Parse("2024-10-02"),
                     UpdatedAt = DateTime.Parse("2024-10-02"),
+                     EntryDate = new DateTime(2024, 11, 1), // 1st November 2024
+            ExpiryDate = new DateTime(2024, 11, 28) // 28th November 2024
                 },
 
                 new Product()
@@ -310,6 +338,8 @@ namespace PureFood.Data.Configurations
                     Status = true,
                     CreatedAt = DateTime.Parse("2024-10-02"),
                     UpdatedAt = DateTime.Parse("2024-10-02"),
+                     EntryDate = new DateTime(2024, 11, 2), // 2nd November 2024
+            ExpiryDate = new DateTime(2024, 11, 29) // 29th November 2024
                 },
 
                 new Product()
@@ -330,6 +360,8 @@ namespace PureFood.Data.Configurations
                     Status = true,
                     CreatedAt = DateTime.Parse("2024-10-02"),
                     UpdatedAt = DateTime.Parse("2024-10-02"),
+                       EntryDate = new DateTime(2024, 11, 3), // 3rd November 2024
+            ExpiryDate = new DateTime(2024, 11, 30) // 30th November 2024
                 },
 
                 new Product()
@@ -350,6 +382,8 @@ namespace PureFood.Data.Configurations
                     Status = true,
                     CreatedAt = DateTime.Parse("2024-10-02"),
                     UpdatedAt = DateTime.Parse("2024-10-02"),
+                      EntryDate = new DateTime(2024, 11, 4), // 4th November 2024
+            ExpiryDate = new DateTime(2024, 12, 1) // 1st December 2024
                 },
 
                 new Product()
@@ -370,6 +404,8 @@ namespace PureFood.Data.Configurations
                     Status = true,
                     CreatedAt = DateTime.Parse("2024-10-02"),
                     UpdatedAt = DateTime.Parse("2024-10-02"),
+                          EntryDate = new DateTime(2024, 11, 5), // 5th November 2024
+            ExpiryDate = new DateTime(2024, 12, 2) // 2nd December 2024
                 },
 
                 new Product()
@@ -390,6 +426,8 @@ namespace PureFood.Data.Configurations
                     Status = true,
                     CreatedAt = DateTime.Parse("2024-10-02"),
                     UpdatedAt = DateTime.Parse("2024-10-02"),
+                      EntryDate = new DateTime(2024, 11, 6), // 6th November 2024
+            ExpiryDate = new DateTime(2024, 12, 3) // 3rd December 2024
                 },
 
                 new Product()
@@ -410,6 +448,8 @@ namespace PureFood.Data.Configurations
                     Status = true,
                     CreatedAt = DateTime.Parse("2024-10-02"),
                     UpdatedAt = DateTime.Parse("2024-10-02"),
+                      EntryDate = new DateTime(2024, 11, 7), // 7th November 2024
+            ExpiryDate = new DateTime(2024, 12, 4) // 4th December 2024
                 },
                 
                 // product 21
---
+++ PureFood.Data/Migrations/20241004150415_FixExpireTimeForProduct.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PureFood.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixExpireTimeForProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("0e758eaa-331b-409a-b09a-335cdb16a1f5"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(2024, 10, 19, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 11, 15, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("25134650-be13-43de-bc40-22a9ea54b543"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(2024, 10, 26, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 11, 22, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("2adef94c-fa06-4369-89ab-34e71052368e"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(2024, 11, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 11, 28, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("30cfb1b3-da69-4a84-b94c-07ffe79597e0"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(2024, 10, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 11, 17, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("487ca896-ab72-4735-bee9-1b6a42ec3be6"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(2024, 10, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 11, 16, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("576aa3a9-49ff-486c-8efc-a0548ee90ee7"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(2024, 11, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 12, 2, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("6404ba59-9c63-4192-889b-3448cd7c9ba0"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(2024, 10, 29, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 11, 25, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("6b821109-b650-4fa6-be2a-36dc8d5fbb4d"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(2024, 11, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 11, 29, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("6eeb6843-acf8-4760-b1b3-7ff01e5d3e8f"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(2024, 10, 24, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 11, 20, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("74bc7dde-d01e-46ca-8fa6-e7cb7a70bb31"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(2024, 10, 31, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 11, 27, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("7a684d3b-8e9f-4f06-a767-d59c1953f249"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(2024, 10, 30, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 11, 26, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("8212e748-fe88-40af-894e-708a376f4fa7"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(2024, 10, 28, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 11, 24, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("8b23df0f-ed33-48c5-9bfa-b2f15a6e6d0f"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(2024, 10, 27, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 11, 23, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("a5c11a08-497c-4846-b922-bb8a95078dd1"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(2024, 10, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 11, 21, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("a69cd06b-8762-49cc-9228-1fc88956fb3c"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(2024, 11, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 12, 3, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("b39c0722-44aa-49e5-8ff9-b9ad9a9594fc"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(2024, 11, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 12, 4, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("ba9968e6-4ad4-406c-9268-28acd686f73c"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(2024, 11, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 12, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("d3fd8b9e-ff11-4c14-a453-edfd58f05135"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(2024, 10, 23, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 11, 19, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("e4cdecb7-84f2-4c28-be13-76b4f771b6f5"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(2024, 10, 22, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 11, 18, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("fc727800-963c-4305-93a0-3b27dfb1d734"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(2024, 11, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 11, 30, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("0e758eaa-331b-409a-b09a-335cdb16a1f5"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("25134650-be13-43de-bc40-22a9ea54b543"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("2adef94c-fa06-4369-89ab-34e71052368e"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("30cfb1b3-da69-4a84-b94c-07ffe79597e0"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("487ca896-ab72-4735-bee9-1b6a42ec3be6"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("576aa3a9-49ff-486c-8efc-a0548ee90ee7"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("6404ba59-9c63-4192-889b-3448cd7c9ba0"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("6b821109-b650-4fa6-be2a-36dc8d5fbb4d"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("6eeb6843-acf8-4760-b1b3-7ff01e5d3e8f"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("74bc7dde-d01e-46ca-8fa6-e7cb7a70bb31"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("7a684d3b-8e9f-4f06-a767-d59c1953f249"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("8212e748-fe88-40af-894e-708a376f4fa7"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("8b23df0f-ed33-48c5-9bfa-b2f15a6e6d0f"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("a5c11a08-497c-4846-b922-bb8a95078dd1"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("a69cd06b-8762-49cc-9228-1fc88956fb3c"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("b39c0722-44aa-49e5-8ff9-b9ad9a9594fc"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("ba9968e6-4ad4-406c-9268-28acd686f73c"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("d3fd8b9e-ff11-4c14-a453-edfd58f05135"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("e4cdecb7-84f2-4c28-be13-76b4f771b6f5"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: new Guid("fc727800-963c-4305-93a0-3b27dfb1d734"),
+                columns: new[] { "EntryDate", "ExpiryDate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+        }
+    }
+}
